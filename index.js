6dc014511d4870728243c794df518ee7515e1560
myf ma <myf@worldtalks.org>
2015-03-06 19:32:43 -0500

    testing out child_process

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var Songza = require('songza');
+var cp = require('child_process');
+var util = require('util');
 var settings = require('./settings');
 var songza = new Songza(settings);
 var promise = songza.station.nextSong(1411908);
 promise.then(function(res) {
-    console.log("here I got the result!", res);
-    debugger;
+    //console.log("here I got the result!", res);
+    console.log("the title is", res.song.title);
+    cp.spawn("mplayer", [res.listen_url]);
 });
