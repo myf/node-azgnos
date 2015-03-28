d5c2a3ffc747479e2a704de06de78ee974e8bdf7
myf ma <myf@worldtalks.org>
2015-03-28 17:52:57 -0400

    checkin to migrate work station

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ var cp = require('child_process');
 var util = require('util');
 var settings = require('./settings');
 var songza = new Songza(settings);
-var promise = songza.station.nextSong(1411908);
-promise.then(function(res) {
-    //console.log("here I got the result!", res);
-    console.log("the title is", res.song.title);
-    cp.spawn("mplayer", [res.listen_url]);
+//var promise = songza.station.nextSong(1411908);
+//promise.then(function(res) {
+//    //console.log("here I got the result!", res);
+//    console.log("the title is", res.song.title);
+//    //cp.spawn("mplayer", ["-really-quiet", res.listen_url]);
+//});
+
+var query = "folk";
+var station = songza.search.station(query);
+station.then(function(res){
+    console.log(res);
+    process.exit(0);
 });
