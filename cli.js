a9f921324a6190b1bd4c77dbe83dc8ee304545ec
myf <myf@worldtalks.org>
2015-09-22 22:23:19 -0400

    terminal-menu unpiped success, due to the fact that menu.on(close) does more stuff so it needs to be nested in menu.on(select)

--- cli.js
+++ cli.js
@@ -1,45 +1,60 @@
-var Menu = require('terminal-menu');
-var prompt = require('prompt');
+var Menu = require('terminal-menu'); 
+//var prompt = require('prompt');
 var azgnos = require('./api');
-var menu = Menu({ width: 80, x: 4, y: 2 });
-var menu_init = function(menu) {
+var readline = require('readline');
+var menu_init = function(list, cb) {
+    var menu = new Menu({ width: 80, x: 4, y: 2 });
+    var menu_stream = menu.createStream();
+    process.stdin.setRawMode(true);
+    process.stdin
+        .pipe(menu_stream)
+        .pipe(process.stdout);
     menu.reset();
     menu.write('AZGNOS TERMINAL\n');
     menu.write('-------------------------\n');
-};
-menu_init(menu);
-menu.add('SEARCH MUSIC GENRE');
-menu.add('EXIT')
- 
-var label_action = function(label, menu) {
-    if (label === 'SEARCH MUSIC GENRE') {
-        prompt.get(["genre"], function(err, data) {
-            azgnos.search_station(data['genre'], function(res) {
-                if (res.length) {
-                    var name_list = res.map(function(item){return item.name});
-                    menu_init(menu);
-                    name_list.map(function(item){
-                        menu.add(item);
-                    });
-                }
-            })
-        })
-    };
-};
-
-menu.on('select', function (label) {
-    if (label === 'EXIT') {
-        console.log('See ya!');
+    list.map(function(item){
+        menu.add(item);
+    });
+    menu.write('-------------------------\n');
+    menu.add('EXIT');
+    menu.on('select', function(label) {
+        menu.on('close', function () {
+            process.stdin.setRawMode(false);
+            menu_stream.unpipe(process.stdout);
+            process.stdin.unpipe(menu_stream);
+            if (label === 'EXIT') {
+                console.log('See ya!');
+            } else {
+                cb(label);
+            }
+        });
         menu.close();
-    } else {
-        label_action(label, menu);
-    }
-});
-process.stdin.pipe(menu.createStream()).pipe(process.stdout);
+    });
+};
 
- 
-process.stdin.setRawMode(true);
-menu.on('close', function () {
-    process.stdin.setRawMode(false);
-    process.stdin.end();
+menu_init(['SEARCH MUSIC GENRE'], function(label) {
+        label_action(label);
 });
+
+var label_action = function(label) {
+    if (label === 'SEARCH MUSIC GENRE') {
+        get_genre();
+    }
+};
+
+var get_genre = function() {
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question('what is the genre then?\n', function(genre) {
+        azgnos.search_station(genre, function(res) {
+            if (res.length) {
+                var name_list = res.map(function(item){return item.name;});
+                menu_init(name_list, function(label) {
+                    listen_station(label);
+                });
+            }
+        });
+    });
+};
