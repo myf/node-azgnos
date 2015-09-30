7d3a18f5224fa388bd5cac237e748cbbc15e39f8
myf <myf@worldtalks.org>
2015-09-30 18:21:12 +0800

    sub menu working

--- cli.js
+++ cli.js
@@ -1,40 +1,39 @@
-var Menu = require('terminal-menu'); 
-//var prompt = require('prompt');
+var createMenu = require('simple-terminal-menu'); 
 var azgnos = require('./api');
 var readline = require('readline');
-var menu_init = function(list, cb) {
-    var menu = new Menu({ width: 80, x: 4, y: 2 });
-    var menu_stream = menu.createStream();
-    process.stdin.setRawMode(true);
-    process.stdin
-        .pipe(menu_stream)
-        .pipe(process.stdout);
-    menu.reset();
-    menu.write('AZGNOS TERMINAL\n');
-    menu.write('-------------------------\n');
-    list.map(function(item){
-        menu.add(item);
+var main_menu = function() {
+    var menu = createMenu({
+        x: 4,
+        y: 2
     });
-    menu.write('-------------------------\n');
-    menu.add('EXIT');
-    menu.on('select', function(label) {
-        menu.on('close', function () {
-            process.stdin.setRawMode(false);
-            menu_stream.unpipe(process.stdout);
-            process.stdin.unpipe(menu_stream);
-            if (label === 'EXIT') {
-                console.log('See ya!');
-            } else {
-                cb(label);
-            }
-        });
+    menu.writeLine('AZGNOS TERMINAL');
+    menu.writeSeparator();
+    menu.add('SEARCH MUSIC GENRE', get_genre);
+    menu.writeSeparator();
+    menu.add('EXIT', function() {
+        console.log('See ya');
         menu.close();
     });
+         
 };
 
-menu_init(['SEARCH MUSIC GENRE'], function(label) {
-        label_action(label);
-});
+var menu_template = function(list, cb) {
+    var menu = createMenu({
+        x: 4,
+        y: 2
+    });
+    menu.writeLine('AZGNOS TERMINAL');
+    menu.writeSeparator();
+    list.map(function(item) {
+        menu.add(item, cb);
+    });
+    menu.writeSeparator();
+    menu.add('EXIT', function() {
+        console.log('See ya');
+        menu.close();
+    });
+         
+};
 
 var label_action = function(label) {
     if (label === 'SEARCH MUSIC GENRE') {
@@ -51,10 +50,13 @@ var get_genre = function() {
         azgnos.search_station(genre, function(res) {
             if (res.length) {
                 var name_list = res.map(function(item){return item.name;});
-                menu_init(name_list, function(label) {
+                menu_template(name_list, function(label) {
+                    console.log(label);
                     listen_station(label);
                 });
             }
         });
     });
 };
+
+main_menu();
